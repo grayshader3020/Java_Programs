4f211224068a01882d7e66c6a167f1ae0e0c8302
Advait Vinayak Zende <130883862+grayshader3020@users.noreply.github.com>
2025-02-07 13:08:17 +0530

    Create DelCode.java

---
+++ 11_Miscellaneous/DelCode.java
@@ -0,0 +1,8 @@
+class DelCode{
+	public static void main(String[] args) {
+		int num = 5;
+		int count = 15;
+		/*1 2 3 4 5 1 2 3 4 5 1 2 3 4 5 1 2 3 4 5*/
+		System.out.println((count%num)+1);
+	}
+}
