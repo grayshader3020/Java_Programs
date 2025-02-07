a7bc9cb8dfbc18ff00725e4aecd8aeadeb2b57ea
Advait Vinayak Zende <130883862+grayshader3020@users.noreply.github.com>
2025-02-07 12:48:07 +0530

    Create Intro.java

---
+++ 01_Basics/Intro.java
@@ -0,0 +1,11 @@
+public class Intro{
+	public static void main(String[] args){
+		System.out.println("Hi My name is Advait");
+		System.out.println("I am pursuing my undergrad degree in Computer Science at ADCET,ashta Dist.Sangli");
+		System.out.println("I am in final year of my college.");
+		System.out.println("Talking about my hobbies , I love computers, I am intersted in Cybersecurity especially");
+		System.out.println("I completed my college at Defence Carrer Academy Aurangabad");
+		System.out.println("I completed my schooling at Kirloskar Highschool kirloskarwadi");
+		System.out.println("Thats all about me :)");
+}
+}
