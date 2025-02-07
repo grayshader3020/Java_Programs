8860520e7682f3d35cb939d4d660df0aaf89aba2
Advait Vinayak Zende <130883862+grayshader3020@users.noreply.github.com>
2025-02-07 13:09:57 +0530

    Create Alphabets.java

---
+++ 10_Loops/Alphabets.java
@@ -0,0 +1,11 @@
+class Alphabets{
+	public static void main(String[] args) {
+		int i;
+		System.out.println("Number upto 100 are: ");
+		i=1;
+		while(i<=100){
+			System.out.println(i+" ");
+			i++;
+		}
+	}
+}
