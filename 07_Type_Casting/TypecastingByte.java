04605bb0297a8277cbfc021471c06931666df380
Advait Vinayak Zende <130883862+grayshader3020@users.noreply.github.com>
2025-02-07 13:02:11 +0530

    Create TypecastingByte.java

---
+++ 07_Type_Casting/TypecastingByte.java
@@ -0,0 +1,16 @@
+class TypecastingByte{
+	public static void main(String[] args) {
+		short a = 500 ;
+		System.out.println("short a: "+ a);
+		char b = 500 ;
+		System.out.println("char b: " +b);
+		int c = 500;
+		System.out.println("int c: " + c);
+		long d = 500;
+		System.out.println("long d: " + d);
+		float e = 500;
+		System.out.println("float e: " + e);
+		double f = 500;
+		System.out.println("double f: " +f);
+	}
+}
