7cdeb95c8ccadf62aae12215602bea6eddba2311
Advait Vinayak Zende <130883862+grayshader3020@users.noreply.github.com>
2025-02-07 12:52:41 +0530

    Create Add1.java

---
+++ 03_Operators/Arithmetic/Add1.java
@@ -0,0 +1,58 @@
+class Add1{
+	public static void main(String[] args){
+		/*byte with byte and storing in byte
+		byte a =20;
+		byte b= 10;
+		byte res= a+b;
+		System.out.println(res);  possible lossy conversion from int to byte int is allowed */
+
+	   /* short with short  and storing in short 
+		short a = 500;
+		short b = 200;
+		int res = a+b;
+		System.out.println(res); possible lossy conversion from int to short conclusion: store in int*/
+
+		/* char with char and storing in char
+		char a = 'a';
+		char b = 'b';
+		int res = a+b;
+		System.out.println(res); //:+ possible lossy conversion from int to char conclusion store the ascii value returned in int*/
+
+
+		/*int to int and storing in int 
+		int a = 9000;
+		int b = 2000;
+		int res = a+b;
+		System.out.println(res); // sucessful execution */
+
+		/*long with long and storing in long 
+		long a = 25000l;
+		long b= 250l;
+		long res = a+b;
+		System.out.println(res); sucessful execution */
+
+		/*float with float and storing with float
+		float a = 2.2f;
+	    float b = 1.1f;
+	    float res = a+b;
+	    System.out.println(res); sucessful execution */ 
+
+	    /*double with double and storing in double
+	    double a = 2.2;
+	    double b = 1.1;
+	    double res = a+b;
+	    System.out.println(res); sucessful execution*/
+
+	    /*string with string and storing in string
+	    String a = "sql";
+	    String b = "db";
+	    String res = a+b;
+	    System.out.println(res); bad operand types for binary operator '+' CTE*/
+
+	    // for boolean it is compile time error
+
+
+
+
+	}
+}
