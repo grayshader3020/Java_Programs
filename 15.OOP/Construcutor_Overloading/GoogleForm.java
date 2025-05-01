af63c29f4f761e9276846b9489cdb444da315b74
grayshader3020 <advaitzende2003@gmail.com>
2025-05-01 12:37:37 +0530

    commit regular

--- 15.OOP/Construcutor_Overloading/GoogleForm.java
+++ 15.OOP/Construcutor_Overloading/GoogleForm.java
@@ -1,4 +1,60 @@
 package Construcutor_Overloading;
 
-public class GoogleForm {
+public class GoogleForm{
+    String name;
+    long phoneNumber;
+    long telephoneNumber;
+    char gender;
+    String bloodGroup;
+    String email;
+    String dob;
+
+    GoogleForm(){
+
+    }
+    GoogleForm(String name,char gender, String dob)
+    {
+        this.name=name;
+        this.gender=gender;
+        this.dob=dob;
+
+    }
+    GoogleForm(String name, long phoneNumber,char gender, String dob)
+    {
+        this.name=name;
+        this.gender=gender;
+        this.dob=dob;
+
+        //extra
+        this.phoneNumber=phoneNumber;
+    }
+    GoogleForm(String name,long phoneNumber,char gender, String dob,String bloodGroup)
+    {
+        this.name=name;
+        this.gender=gender;
+        this.dob=dob;
+        //extra
+        this.phoneNumber=phoneNumber;
+        this.bloodGroup=bloodGroup;
+    }
+    GoogleForm(String name,long phoneNumber,char gender, String dob,String bloodGroup,String email)
+    {
+        this.name=name;
+        this.gender=gender;
+        this.dob=dob;
+        //extra
+        this.phoneNumber=phoneNumber;
+        this.bloodGroup=bloodGroup;
+        this.email=email;
+    }
+
+    public void displayForm()
+    {
+        System.out.println("Name: "+name);
+        System.out.println("Gender: "+gender);
+        System.out.println("DOB: "+dob);
+        System.out.println("Phone Number: "+phoneNumber);
+        System.out.println("Email: "+email);
+        System.out.println("Blood Group: "+bloodGroup);
+    }
 }
---
+++ 15.OOP/Construcutor_Overloading/GoogleFormDriver.java
@@ -0,0 +1,8 @@
+package Construcutor_Overloading;
+
+public class GoogleFormDriver {
+    public static void main(String[] args) {
+        GoogleForm obj = new GoogleForm("James Bond",'M',"19/01/2002");
+        obj.displayForm();
+    }
+}
